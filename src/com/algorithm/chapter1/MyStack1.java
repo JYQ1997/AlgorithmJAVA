902ac63e147d92ed6055d7de032fb5a919dc28f4
姬永强 <1963590811@qq.com>
2020-01-20 16:30:54 +0800

    初始文件

---
+++ src/com/algorithm/chapter1/MyStack1.java
@@ -0,0 +1,13 @@
+package com.algorithm.chapter1;
+
+/**
+ * 设计一个有getMain功能的栈
+ * @Author YongQiang
+ * @Date 2020/1/20 16:22
+ * @Version 1.0
+ */
+public class MyStack1 {
+    public static void main(String[] args) {
+
+    }
+}
